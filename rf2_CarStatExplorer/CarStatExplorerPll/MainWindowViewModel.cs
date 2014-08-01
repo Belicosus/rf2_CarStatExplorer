95d1a1a1446b3f19cdc0388269e6f92f020aa281
Casper Hougaard <casperhgrd@gmail.com>
2014-08-01 17:29:10 +0200

    The rest of the prjects

---
+++ rf2_CarStatExplorer/CarStatExplorerPll/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarStatExplorer.Pll
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public event EventHandler<CancelEventArgs> CloseApplicationRequested;
+
+    }
+}
---
+++ rf2_CarStatExplorer/CarStatExplorerUiClient/Views/EngineGraphView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CarStatExplorerUiClient.Views
+{
+    /// <summary>
+    /// Interaction logic for EngineGraphView.xaml
+    /// </summary>
+    public partial class EngineGraphView : UserControl
+    {
+        public EngineGraphView()
+        {
+            InitializeComponent();
+        }
+    }
+}

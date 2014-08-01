b21fa1de1df5a5bdfd2573a00a889a2380e7ef07
Casper Hougaard <casperhgrd@gmail.com>
2014-08-01 18:39:47 +0200

    Not quiet working.. don't know why

--- rf2_CarStatExplorer/CarStatExplorerPll/MainWindowViewModel.cs
+++ rf2_CarStatExplorer/CarStatExplorerPll/MainWindowViewModel.cs
@@ -11,5 +11,22 @@ namespace CarStatExplorer.Pll
     {
         public event EventHandler<CancelEventArgs> CloseApplicationRequested;
 
+        private EngineGraphViewModel _EngineGraphViewModel;
+        public EngineGraphViewModel EngineGraphViewModel
+        {
+            get { return _EngineGraphViewModel; }
+            set
+            {
+                SetPropertyValue(ref _EngineGraphViewModel, value);
+                _EngineGraphViewModel = value;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            _EngineGraphViewModel = new EngineGraphViewModel();   
+
+        }
+
     }
 }
